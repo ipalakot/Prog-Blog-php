b2677b4418d61c26d662ee79012d6d8d2e24a5e8
ipalakot <ipalakot@gmail.com>
2019-04-17 21:22:27 +0200

    Insertion Bootstrap dand Form

--- index.php
+++ index.php
@@ -1,16 +1,25 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" rel="stylesheet" >
+</head>
 
-    require'form.php';
 
-        $form = new Form($_POST);
+<body>
+    <?php
+        require'form.php';
+        require 'BootstrapForm.php';
+        $form = new BootstrapForm($_POST);
+    ?>
 
-?>
+  <form action="#"  method="post">
 
-<form action="#"  method="post">
     <?php
         echo $form -> input('username');
         echo $form -> input('password');
         echo $form -> submit();
-
     ?>
-</form>
+  </form>
+</body>
