cf218cc444daf2bbef8714b83415753d09931e55
ipalakot <ipalakot@gmail.com>
2019-04-18 22:57:19 +0200

    connexion DataBase

--- pages/home.php
+++ pages/home.php
@@ -1,3 +1,8 @@
-My home page
-<p></p>
-<a href="index.php?p=single">Single</a>
+<?php
+
+$pdo = new PDO('mysql:dbname=blog;host=localhost', 'root','');
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+$count= $pdo->exec('INSERT INTO  articles SET titre ="Mon Titre", date="' .date('Y-m-d H:i:s'). '"');
+
+
+?>
