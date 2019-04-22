a7eb547142e1327e9c269893a29cdd71b841896a
ipalakot <ipalakot@gmail.com>
2019-04-22 17:07:00 +0200

    Last Articles

--- pages/home.php
+++ pages/home.php
@@ -1,9 +1,8 @@
 <ul>
-    <?php foreach (App\App::getDb()->query('SELECT * FROM articles', 'App\Table\Article') as $post): ?>
-
-
+    <?php foreach (\App\Table\Article::getLast() as $post): ?>
 
         <h2> <a href="<?= $post->url ?>"> <?= $post->titre; ?> </a> </h2>
+
         <p><?= $post->extrait; ?></p>
 
 
