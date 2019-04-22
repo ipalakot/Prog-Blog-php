abf7c6a08f99d4eafe045daa3f0365f443f73209
ipalakot <ipalakot@gmail.com>
2019-04-22 16:54:36 +0200

    Recuperation de last articles

--- app/Table/Article.php
+++ app/Table/Article.php
@@ -2,8 +2,14 @@
 
 namespace App\Table;
 
+use  App\App;
+
 
  class Article{
+
+     public static function getLast(){
+         return App::getDb()->query('SELECT * FROM articles', __CLASS__);
+     }
 
      public function __get($key){
 
